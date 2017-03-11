62d9d5cdd3e9bdf2a69fc149ed0ef5ccf6680780
Alberto Diaz <aado29@gmail.com>
2017-03-11 02:17:54 -0400

    update app container component

--- src/client/components/App/App.jsx
+++ src/client/components/App/App.jsx
@@ -2,84 +2,59 @@ import React, { Component, PropTypes }      from 'react';
 import { connect }                          from 'react-redux';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {blue500, blue700, blue100, orangeA700, grey900, grey800, grey300} from 'material-ui/styles/colors';
+import {orangeA700, grey900, grey800, grey300} from 'material-ui/styles/colors';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import style from './css/style.css';
-import muiThemeable from 'material-ui/styles/muiThemeable';
-
 
 import Menu from '../Menu';
 import Frontpage from '../Frontpage';
-import Footer from '../Footer';
 
 const propTypes = {
-  dispatch: PropTypes.func,
-  userAgent: PropTypes.string,
-  children: PropTypes.node,
+	dispatch: PropTypes.func,
+	userAgent: PropTypes.string,
+	children: PropTypes.node,
 };
 
 injectTapEventPlugin();
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-render() {
+	render() {
+		const muiTheme = getMuiTheme({
+			palette: {
+					primary1Color: orangeA700,
+					primary2Color: grey900,
+					primary3Color: grey300,
+					canvasColor: grey300
+			},
+			appBar: {
+				height: 70,
+			},
+		});
 
-  // const muiTheme = getMuiTheme({
-  //     palette: {
-  //       primary1Color: orangeA700,
-  //       primary2Color: grey900,
-  //       primary3Color: grey300
-  //     }
-  //   }, {
-  //     avatar: {
-  //       borderColor: null
-  //     },
-  //     userAgent: this.props.userAgent
-  //   });
-  const muiTheme = getMuiTheme({
-  palette: {
-      primary1Color: orangeA700,
-      primary2Color: grey900,
-      primary3Color: grey300,
-      canvasColor: grey300
-  },
-  appBar: {
-    height: 70,
-  },
-});
-
-    const Main= {
-      //paddingBottom: '160px' // footer height size + 60px
-      height:"calc(100vh + 200px)"
-    }
-
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={{ position:"relative" }}>
-          <div>
-            <Menu/>
-          </div>
-          <div style={{background:muiTheme.palette.primary3Color,paddingBottom:'160px', marginTop:71, minHeight:'100vh'}}>
-            {this.props.children}
-          </div>
-          <Footer/>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+		return (
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<div style={{ position:"relative" }}>
+					<Menu/>
+					<div style={{marginTop:muiTheme.appBar.height}}>
+						{this.props.children}
+					</div>
+				</div>
+			</MuiThemeProvider>
+		);
+		}
 
 }
 
 App.propTypes = propTypes;
 
 function mapStateToProps(state) {
-  return {
-    userAgent: state.userAgent
-  };
+	return {
+		userAgent: state.userAgent
+	};
 }
 
 export default connect(mapStateToProps)(App);

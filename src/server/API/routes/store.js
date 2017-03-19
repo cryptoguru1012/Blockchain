8b5d019d61afb3918d80d18a04506e81e72b51d5
Nelbi Alvarado <joseegnzalez@gmail.com>
2017-03-19 17:13:34 -0400

    Update checkAuth on categories - Removed checkAuth from \src\server \API\routes\store.js (Not needed for now).

--- src/server/API/routes/store.js
+++ src/server/API/routes/store.js
@@ -1,40 +1,46 @@
-import checkAuth from '../helpers/check_auth';
+/* import checkAuth from '../helpers/check_auth';*/
 import createItem from '../controllers/store/create_item';
 import getCategories from '../controllers/store/get_categories';
 import getLastStoreItems from '../controllers/store/get_last_items';
 
 const storeRoutes = (app) => {
-    app.post('/API/store/item', checkAuth, (req, res, next) => {
-        req.body.productVideo = req.files.productVideo;
-        req.body.userId = req.user._id;
-        createItem(req.body, (err, data) => {
-            if (err) {
-                return next(err);
-            }
+  app.post(
+    '/API/store/item',
+    /* checkAuth,*/ (req, res, next) => {
+      req.body.productVideo = req.files.productVideo;
+      req.body.userId = req.user._id;
+      createItem(req.body, (err, data) => {
+        if (err) {
+          return next(err);
+        }
 
-            return res.status(data.status).send(data);
-        });
-    });
-    
-    app.get('/API/store/products', (req, res, next) => {
-        getLastStoreItems((err, data) => {
-            if (err) {
-                return next(err);
-            }
+        return res.status(data.status).send(data);
+      });
+    },
+  );
 
-            res.status(data.status).send(data);
-        });
-    });
-    
-    app.get('/API/store/categories', checkAuth, (req, res, next) => {
-        getCategories((err, data) => {
-            if (err) {
-                return next(err);
-            }
+  app.get('/API/store/products', (req, res, next) => {
+    getLastStoreItems((err, data) => {
+      if (err) {
+        return next(err);
+      }
 
-            return res.status(data.status).send(data);
-        });
+      res.status(data.status).send(data);
     });
+  });
+
+  app.get(
+    '/API/store/categories',
+    /* checkAuth,*/ (req, res, next) => {
+      getCategories((err, data) => {
+        if (err) {
+          return next(err);
+        }
+
+        return res.status(data.status).send(data);
+      });
+    },
+  );
 };
 
 export default storeRoutes;

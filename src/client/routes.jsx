242f1e8d505d79f44b262f15b3c5aaaebfb5e699
Alberto Diaz <aado29@gmail.com>
2017-04-09 18:31:39 -0400

    added root Browser component to routes

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -10,6 +10,7 @@ import App                          from './components/App';
 import Register                     from './components/Register';
 import Frontpage                    from './components/Frontpage';
 import Store                        from './components/Store';
+import Browser                      from './components/Browser';
 
 const Routes = (
   <Router render={(props) => <ReduxAsyncConnect {...props}/>} history={browserHistory}>
@@ -17,6 +18,7 @@ const Routes = (
       <IndexRoute component={Frontpage} />
       <Route path='/register' component={Register} />
       <Route path='/store/newItem' component={Store.NewItem} />
+      <Route path='/browser' component={Browser} />
     </Route>
   </Router>
 );

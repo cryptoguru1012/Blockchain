dbe104b23f488df4e843fa2eba2cefdfe21c32a5
Alberto Diaz <aado29@gmail.com>
2017-04-14 00:18:11 -0400

    added player component

--- src/client/components/Offer/OfferViewSuccess.jsx
+++ src/client/components/Offer/OfferViewSuccess.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
+import createFragment from 'react-addons-create-fragment';
 
 // components
 import { Row, Col, Grid, Button } from 'react-bootstrap';
+import VideoPlayer from '../Store/VideoPlayer';
+
 
 class OfferViewSuccess extends React.Component {
 	constructor(props) {
 		super(props);
-		
+
+		let description = this.props.data.description;
+		this.state = {
+			description: JSON.parse(description)
+		}
 	}
 
 	render() {
-		let data = JSON.parse(this.props.data.description)
-			, description = data.text
-			, urlVideo = data.urlVideo
-			, subtitlesVideo = data.subtitlesVideo
-
 		return (
 			<Col xs={12}>
 				<h2>{this.props.data.title}</h2>
+				<VideoPlayer
+					url={this.state.description.urlVideo}
+					subtitles={this.state.description.subtitlesVideo}
+				/>
 			</Col>
 		);
 	}

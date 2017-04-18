8b65c381468513e48a0558e33798a1a2fb6c01d0
Alberto Diaz <aado29@gmail.com>
2017-04-18 03:45:51 -0400

    added component

---
+++ src/client/components/Browser/GaleryItemBrowser.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// components
+import { Row, Col, Grid, Button, Glyphicon } from 'react-bootstrap';
+
+class GaleryItemBrowser extends React.Component {
+	constructor(props) {
+		super(props);
+		this.renderSlide = this.renderSlide.bind(this);
+		console.log('run GaleryItemBrowser');
+	}
+
+	renderSlide() {
+		return this.props.images.map((image, i) => {
+			return (
+				<img style={{width: '100%'}} key={i} src={image} />
+			)
+		})
+	}
+
+	render() {
+		return (
+			<Row>
+				<Col xs={12}>
+					{this.renderSlide()}
+				</Col>
+			</Row>
+		);
+	}
+}
+
+export default GaleryItemBrowser;

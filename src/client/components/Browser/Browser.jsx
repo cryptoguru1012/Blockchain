012aada842364e53bff180b104646bb2d1f74d61
Alberto Diaz <aado29@gmail.com>
2017-04-23 03:30:38 -0400

    added component (FilterBrowser) to render component

--- src/client/components/Browser/Browser.jsx
+++ src/client/components/Browser/Browser.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Row, Col, Grid, Button, Glyphicon } from 'react-bootstrap';
 
-import { search } from '../../redux/actions/browser';
+import { setFilter, search } from '../../redux/actions/browser';
 
 import FormBrowser from './FormBrowser';
+import FilterBrowser from './FilterBrowser';
 import ListBrowser from './ListBrowser';
+
+let filters = [
+	{
+		value: 'currency',
+		name: 'Currency'
+	}, 
+	{
+		value: 'title',
+		name: 'Name'
+	},
+	{
+		value: 'geolocation',
+		name: 'Geolocation'
+	},
+	{
+		value: 'paymentoptions_display',
+		name: 'Payment options'
+	},
+	{
+		value: 'category',
+		name: 'Category'
+	}
+]
 
 class Browser extends React.Component {
 	constructor(props) {
@@ -17,6 +41,7 @@ class Browser extends React.Component {
 			<Grid>
 				<Col xs={12}>
 					<FormBrowser onSearch={this.props.onSearch} browser={this.props.browser} />
+					<FilterBrowser filters={filters} onFilter={this.props.onFilter}/>
 					<ListBrowser items={this.props.browser.items}/>
 				</Col>
 			</Grid>
@@ -35,6 +60,9 @@ function mapDispatchToProps(dispatch) {
 		onSearch: (data) => {
 			dispatch(search(data));
 		},
+		onFilter: (filter) => {
+			dispatch(setFilter(filter));
+		}
 	};
 }
 

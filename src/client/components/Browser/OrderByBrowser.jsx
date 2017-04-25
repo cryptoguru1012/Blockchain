dce7266175cc54f480e8d3ec669ee4ad5dddffc4
Ramyya <ramyya@gmail.com>
2017-04-24 20:28:06 -0700

    Added gridlist for items and selectfield for orderby

--- src/client/components/Browser/OrderByBrowser.jsx
+++ src/client/components/Browser/OrderByBrowser.jsx
@@ -3,16 +3,23 @@ import React from 'react';
 // components
 import { Row, Col, Grid, Button, Glyphicon } from 'react-bootstrap';
 import { FormsySelect } from 'formsy-material-ui/lib';
-import { MenuItem } from 'material-ui';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 class OrderByBrowser extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.renderItemsOption = this.renderItemsOption.bind(this);
+		this.state = {
+			value : ''
+		}
 	}
 
 	handleSubmit(e) {
+		this.setState({
+      		value: e.target.value,
+    	});
 		console.log(e.target);
 		this.props.onOrder(e.target.value);
 	}
@@ -20,7 +27,7 @@ class OrderByBrowser extends React.Component {
 	renderItemsOption() {
 		if (this.props.items.length > 0) {
 			return this.props.items.map((item, i) => {
-				return <option key={i} value={item.value}>{item.name}</option>
+				return <MenuItem key={i} value={item.value}>{item.name}</MenuItem>
 			})
 		}
 	}
@@ -31,11 +38,14 @@ class OrderByBrowser extends React.Component {
 				<Formsy.Form>
 					<Col xs={12}>
 						<Row>
-							<label>Order by: </label> 
-							<select name="item" onChange={e => this.handleSubmit(e)}>
-								<option value="">options</option>
+							<SelectField 
+							fullWidth= {true}
+							floatingLabelText="Order by"
+							name="item" 
+							value={this.state.value}
+							 onChange={e => this.handleSubmit(e)}>
 								{this.renderItemsOption()}
-							</select>
+							</SelectField>
 						</Row>
 					</Col>
 				</Formsy.Form>

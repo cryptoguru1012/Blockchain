49e9c8cdcb4f4df264c16be8cd768361e27a4501
Ramyya <ramyya@gmail.com>
2017-05-20 21:29:27 -0700

    Changes

--- src/client/components/Store/VideoPlayer.jsx
+++ src/client/components/Store/VideoPlayer.jsx
@@ -293,10 +293,7 @@ class VideoPlayer extends React.Component {
 		return (
 			<Row className="video-component" style={styles.videoContainer}>
 				<Col xs={12} md={6} mdOffset={3} lg={6} lgOffset={3}>
-					<div id="poster"/>
-					<video preload="auto" poster={this.state.poster}ref="player" style={styles.video} onMouseLeave={e => handleMouseLeave(e)} onMouseOver={e => handleMouseOver(e)} onClick={this.handleVideoPlay}>
-
-
+					<video poster={this.state.poster} ref="player" style={styles.video} onMouseLeave={e => handleMouseLeave(e)} onMouseOver={e => handleMouseOver(e)} onClick={this.handleVideoPlay}>
 					</video>
 				</Col>
 				{this.renderControls()}

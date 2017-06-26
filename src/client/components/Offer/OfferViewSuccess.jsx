bec0572646f7c8a02fab6df8a6fd11e33b182edb
Ramyya <ramyya@gmail.com>
2017-06-26 07:08:04 -0700

    Rolling back changes

--- src/client/components/Offer/OfferViewSuccess.jsx
+++ src/client/components/Offer/OfferViewSuccess.jsx
@@ -19,31 +19,19 @@ class OfferViewSuccess extends React.Component {
 
 		let description = this.props.data.description;
 		this.state = {
-			description: (isJson(description)) ? JSON.parse(description) : description,
+			description: (isJson(description)) ? JSON.parse(description) : description
 		}
 	}
 
 	rendeDescription() {
-		console.log("redenr fn");
 		return <p>{this.state.description}</p>
 	}
 
-	imageGet() {
-			return this.state.description.match(/https?:\/\/.*\.(?:png|jpg|gif)/g);
-	}
 	render() {
-		
-		let hasImages=''; 
-		if( isJson(this.props.data.description) === false ) {
-			hasImages = this.imageGet();
-		}
-		console.log(this.state.description.urlImage);
-		debugger;
 		return (
 			<Col xs={12}>
 				<h2>{'Title: ' + this.props.data.title}</h2>
 				<h3>{'Price: ' + this.props.data.price + ' ' + this.props.data.currency}</h3>
-				
 				{ this.state.description.urlVideo && isJson(this.props.data.description) && <VideoPlayer
 					url={this.state.description.urlVideo}
 					subtitles={this.state.description.subtitlesVideo}
@@ -51,10 +39,7 @@ class OfferViewSuccess extends React.Component {
 				{ this.state.description.urlImage && isJson(this.props.data.description) && 	<img height="400" width="400"
 					src={this.state.description.urlImage}
 				/> }
-				{ !isJson(this.props.data.description) && this.rendeDescription() && <img height="400" width="400" src={hasImages}/> }
-				<br />
-				<label>Description</label> 
-				<p> {this.state.description}</p>
+				{ !isJson(this.props.data.description) && this.rendeDescription()}
 			</Col>
 		);
 	}

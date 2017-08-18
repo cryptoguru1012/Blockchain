d0ad2a19cc0e5f4c3e1321f7830bed3273a1cbad
VV <viktor.vaczi.90@gmail.com>
2017-08-18 14:49:47 +0200

    Added redux dev tools support: https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en It allows you to see and manipulate the store live.

--- src/client/client.js
+++ src/client/client.js
@@ -4,11 +4,12 @@ import { browserHistory, Router } from 'react-router';
 import routes from './routes';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './redux/reducers/index';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(reduxThunk)));
 
 const component = (
   <Provider store={store}>
